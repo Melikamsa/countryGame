f1d05b6cb7058c01e63ae8f5f63ee59a14f1b159
melika <melikam7413@gmail.com>
2024-06-20 17:59:50 +0330

    create one page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css"
+
+
+export default function Page() {
+    return (
+      <section className="bgAll h-[100vh] flex flex-col items-center">
+        <h1 className="text-white pt-10">countries game</h1>
+        <button className="text-white m-40 bg-[#1465e8] py-4 px-10 rounded-lg">start</button>
+      </section>
+    );
+  }
